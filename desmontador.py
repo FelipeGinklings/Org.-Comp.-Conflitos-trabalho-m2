3bd3fd8f73ae5db1e4b78e3d1f719826f3b8253e
YourUsername <youremail@example.com>
2025-10-21 21:02:00 -0300

    Version 2

--- desmontador.py
+++ desmontador.py
@@ -1,3 +1,54 @@
+def createName(opcode, func3):
+    translate = {
+        "1100011": {
+            "000": "beq",
+            "001": "bne",
+            "100": "blt",
+            "101": "bge",
+            "110": "bltu",
+            "111": "bgeu",
+        },
+        "0100011": {
+            "000": "sb",
+            "001": "sh",
+            "010": "sw",
+        },
+        "0110011": {         
+            "00": "sub",
+            "01": "add",
+            "50": "sra",
+            "51": "srl",
+            "10": "sll",
+            "20": "slt",
+            "30": "sltu",
+            "40": "xor",
+            "60": "or",
+            "70": "and",
+        },
+        "0000011": {
+            "000": "lb",
+            "001": "lh",
+            "010": "lw",
+            "100": "lbu",
+            "101": "lhu",
+        },
+        "0010011-A": {
+            "000": "addi",
+            "010": "slti",
+            "011": "sltiu",
+            "100": "xori",
+            "110": "ori",
+            "111": "andi",
+        },
+        "0010011": {
+            "51": "srai",
+            "50": "srli",
+            "10": "slli",
+        },
+
+    }
+    return translate[opcode][func3]
+
 def cortar_strings(linhas):
     linhas_formatadas = []
 
@@ -32,55 +83,19 @@ def dividir_e_linkar(linhas_divididas):
         match linha[0]:
             case "0010011" if (linha[2] == "001" or linha[2] == "101"):
                 shamt = bin_to_int(resto[-5:])
-                if linha[2] == "101":
-                    if linha[linha[-1][1] == "1"]:
-                        instrucoes.append(f"Formato = I srai | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | shamt = {shamt}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, shamt)) # op_code, rd, func_3, rs1, rs2/shamt/immed
-                    else:
-                        instrucoes.append(f"Formato = I srli | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | shamt = {shamt}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, shamt))
-                if linha[2] == "001":
-                    instrucoes.append(f"Formato = I slli | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | shamt = {shamt}")
-                    estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, shamt))
+                name = createName(linha[0], str(int(linha[2], 2))+str(int(not (linha[linha[-1][1]] == "1"))))
+                instrucoes.append(f"Formato = I {name} | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | shamt = {shamt}")
+                estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, rs2_int))
             case "0010011":
                 immed = bin_to_int(resto, signed=True)
-                match linha[2]:
-                    case "000":
-                        instrucoes.append(f"Formato = I addi | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | imed = {immed}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, immed))
-                    case "010":
-                        instrucoes.append(f"Formato = I slti | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | imed = {immed}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, immed))
-                    case "011":
-                        instrucoes.append(f"Formato = I sltiu | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | imed = {immed}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, immed))
-                    case "100":
-                        instrucoes.append(f"Formato = I xori | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | imed = {immed}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, immed))
-                    case "110":
-                        instrucoes.append(f"Formato = I ori | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | imed = {immed}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, immed))
-                    case "111":
-                        instrucoes.append(f"Formato = I andi | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | imed = {immed}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, immed))
+                name = createName(linha[0]+"-A", linha[2])
+                instrucoes.append(f"Formato = I {name} | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | imed = {immed}")
+                estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, immed))
             case "0000011":
                 immed = bin_to_int(resto, signed=True)
-                match linha[2]:
-                    case "000":
-                        instrucoes.append(f"Formato = I lb | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | imed = {immed}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, immed))
-                    case "001":
-                        instrucoes.append(f"Formato = I lh | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | imed = {immed}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, immed))
-                    case "010":
-                        instrucoes.append(f"Formato = I lw | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | imed = {immed}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, immed))
-                    case "100":
-                        instrucoes.append(f"Formato = I lbu | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | imed = {immed}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, immed))
-                    case "101":
-                        instrucoes.append(f"Formato = I lhu | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | imed = {immed}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, immed))
+                name = createName(linha[0], linha[2])
+                instrucoes.append(f"Formato = I {name} | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | imed = {immed}")
+                estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, immed))
             case "1110011":
                 instrucoes.append(f"Formato = I ecall | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | imed = 0")
                 estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, 0))
@@ -92,39 +107,9 @@ def dividir_e_linkar(linhas_divididas):
                 rs2_bin = resto[-5:]
                 func7 = resto[:-5]
                 rs2_int = bin_to_int(rs2_bin)
-                match linha[2]:
-                    case "000":
-                        if func7 == "0100000":
-                            instrucoes.append(f"Formato = R sub | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | rs2 = x{rs2_int}")
-                            estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, rs2_int))
-                        else:
-                            instrucoes.append(f"Formato = R add | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | rs2 = x{rs2_int}")
-                            estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, rs2_int))
-                    case "101":
-                        if func7 == "0100000":
-                            instrucoes.append(f"Formato = R sra | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | rs2 = x{rs2_int}")
-                            estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, rs2_int))
-                        else:
-                            instrucoes.append(f"Formato = R srl | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | rs2 = x{rs2_int}")
-                            estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, rs2_int))
-                    case "001":
-                        instrucoes.append(f"Formato = R sll | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | rs2 = x{rs2_int}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, rs2_int))
-                    case "010":
-                        instrucoes.append(f"Formato = R slt | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | rs2 = x{rs2_int}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, rs2_int))
-                    case "011":
-                        instrucoes.append(f"Formato = R sltu | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | rs2 = x{rs2_int}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, rs2_int))
-                    case "100":
-                        instrucoes.append(f"Formato = R xor | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | rs2 = x{rs2_int}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, rs2_int))
-                    case "110":
-                        instrucoes.append(f"Formato = R or | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | rs2 = x{rs2_int}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, rs2_int))
-                    case "111":
-                        instrucoes.append(f"Formato = R and | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | rs2 = x{rs2_int}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, rs2_int))
+                name = createName(linha[0], str(int(linha[2], 2))+str(int(not (func7 == "0100000"))))
+                instrucoes.append(f"Formato = R {name} | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | rs2 = x{rs2_int}")
+                estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, rs2_int))
             case "0110111":
                 immed_bin = resto + func_3 + rs1
                 immed = bin_to_int(immed_bin) << 12
@@ -141,17 +126,10 @@ def dividir_e_linkar(linhas_divididas):
                 immed_low = rd  # 5 bits: imm[4:0]
                 rs2_int = bin_to_int(rs2_bin)
                 immed = bin_to_int(immed_high + immed_low, signed=True)
-                match linha[2]:
-                    case "000":
-                        instrucoes.append(f"Formato = S sb | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | rs2 = x{rs2_int} | imed = {immed}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, rs2_int))
-                    case "001":
-                        instrucoes.append(f"Formato = S sh | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | rs2 = x{rs2_int} | imed = {immed}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, rs2_int))
-                    case "010":
-                        instrucoes.append(f"Formato = S sw | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | rs2 = x{rs2_int} | imed = {immed}")
-                        estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, rs2_int))
-            case "1100011":
+                name = createName(linha[0], linha[2])
+                instrucoes.append(f"Formato = S {name} | rd = x{rd_int} | f3 = {func3_int} | rs1 = x{rs1_int} | rs2 = x{rs2_int} | imed = {immed}")
+                estrutura_instrucoes.append((op_code, rd_int, func3_int, rs1_int, rs2_int))
+            case "1100011": # branches
                 rs2_bin = resto[-5:]
                 upper = resto[:-5]  # 7 bits: imm[12] + imm[10:5]
                 immed_12 = upper[0]
@@ -160,26 +138,10 @@ def dividir_e_linkar(linhas_divididas):
                 immed_11 = rd[4]  # 1 bit
                 rs2_int = bin_to_int(rs2_bin)
                 imm_bits = immed_12 + immed_11 + immed_10_5 + immed_4_1
-                immed = bin_to_int(imm_bits + "0", signed=True)
-                match linha[2]:
-                    case "000":
-                        instrucoes.append(f"Formato = B beq | rs1 = x{rs1_int} | rs2 = x{rs2_int} | f3 = {func3_int} | imed = {immed}")
-                        estrutura_instrucoes.append((op_code, 0, func3_int, rs1_int, rs2_int))
-                    case "001":
-                        instrucoes.append(f"Formato = B bne | rs1 = x{rs1_int} | rs2 = x{rs2_int} | f3 = {func3_int} | imed = {immed}")
-                        estrutura_instrucoes.append((op_code, 0, func3_int, rs1_int, rs2_int))
-                    case "100":
-                        instrucoes.append(f"Formato = B blt | rs1 = x{rs1_int} | rs2 = x{rs2_int} | f3 = {func3_int} | imed = {immed}")
-                        estrutura_instrucoes.append((op_code, 0, func3_int, rs1_int, rs2_int))
-                    case "101":
-                        instrucoes.append(f"Formato = B bge | rs1 = x{rs1_int} | rs2 = x{rs2_int} | f3 = {func3_int} | imed = {immed}")
-                        estrutura_instrucoes.append((op_code, 0, func3_int, rs1_int, rs2_int))
-                    case "110":
-                        instrucoes.append(f"Formato = B bltu | rs1 = x{rs1_int} | rs2 = x{rs2_int} | f3 = {func3_int} | imed = {immed}")
-                        estrutura_instrucoes.append((op_code, 0, func3_int, rs1_int, rs2_int))
-                    case "111":
-                        instrucoes.append(f"Formato = B bgeu | rs1 = x{rs1_int} | rs2 = x{rs2_int} | f3 = {func3_int} | imed = {immed}")
-                        estrutura_instrucoes.append((op_code, 0, func3_int, rs1_int, rs2_int))
+                immed = bin_to_int(imm_bits + "0", signed=True)                
+                name = createName(linha[0], linha[2])
+                instrucoes.append(f"Formato = B {name} | rs1 = x{rs1_int} | rs2 = x{rs2_int} | f3 = {func3_int} | imed = {immed}")
+                estrutura_instrucoes.append((op_code, 0, func3_int, rs1_int, rs2_int))
             case "1101111":
                 upper20 = resto + rs1 + func_3  # bits 31..12
                 imm_20 = upper20[0]
